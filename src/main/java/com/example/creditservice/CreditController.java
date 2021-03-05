a39953b9d61a78a2665d151a80a6d79a0a9ded0c
Joe <spaced210@gmail.com>
2021-03-05 08:42:03 -0500

    more code

--- src/main/java/com/example/creditservice/CreditController.java
+++ src/main/java/com/example/creditservice/CreditController.java
@@ -6,9 +6,10 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.server.PathParam;
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -16,42 +17,53 @@ public class CreditController {
 
     private final Logger logger = LogManager.getLogger(CreditController.class);
 
-    private static final String LOOKUP_STR = "Evaluating ";
-    private static final String RETURNING_STR = "";
-
-    private final DataService dataService;
+    private static final String LOOKUP_STR = "Looking up credit info";
+    private static final String RETURNING_STR = "Returning credit info to caller";
 
     @Autowired
-    public CreditController(DataService dataService) {
-        this.dataService = dataService;
+    public CreditController() {
+
     }
 
+    @GetMapping("/credit-score/{customer_id}")
+    public ResponseEntity<Object> getCreditScore(@PathVariable("customer_id") String customer_id) {
+        logger.info(LOOKUP_STR + " for customer ID " + customer_id);
 
-    @PostMapping("/check-credit")
-    public ResponseEntity<Object> getVehicles(@RequestBody CreditCheckRequest creditCheckRequest) {
-        logger.info(String.format("Checking to see if customer '%s' has a high enough " +
-                        "credit score to purchase vehicle '%s'",
-                creditCheckRequest.getCustomerId(),
-                creditCheckRequest.getVehicleId())
-        );
+        int creditScore;
 
-        DataService.Customer customer = dataService.getCustomerById(creditCheckRequest.getCustomerId());
-
-        DataService.Vehicle vehicle = dataService.getVehicleById(creditCheckRequest.getVehicleId());
-
-        if (vehicle.getMinCreditScore() <= customer.getCreditScore()) {
-            logger.info(String.format("Customer '%s' is approved to purchase vehicle '%s'",
-                    creditCheckRequest.getCustomerId(),
-                    creditCheckRequest.getVehicleId())
-            );
-            return new ResponseEntity<>(new CreditCheckResponse(true, customer, vehicle), HttpStatus.OK);
-        } else {
-            logger.info(String.format("Customer '%s' is NOT approved to purchase vehicle '%s'",
-                    creditCheckRequest.getCustomerId(),
-                    creditCheckRequest.getVehicleId())
-            );
-            return new ResponseEntity<>(new CreditCheckResponse(false, customer, vehicle), HttpStatus.OK);
+        switch (customer_id) {
+            case "customer-1":
+                creditScore = 500;
+                break;
+            case "customer-2":
+                creditScore = 489;
+                break;
+            case "customer-3":
+                creditScore = 843;
+                break;
+            case "customer-4":
+                creditScore = 755;
+                break;
+            case "customer-5":
+                creditScore = 687;
+                break;
+            case "customer-6":
+                creditScore = 565;
+                break;
+            case "customer-7":
+                creditScore = 682;
+                break;
+            default:
+                logger.info("No customer found for ID " + customer_id);
+                return new ResponseEntity("No customer found with ID " + customer_id, HttpStatus.NOT_FOUND);
         }
+
+        CreditResponse creditResponse = new CreditResponse();
+        creditResponse.setCreditScore(creditScore);
+
+        logger.info("Returning credit score for customer " + customer_id);
+
+        return new ResponseEntity<>(creditResponse, HttpStatus.OK);
     }
 
 }
---
+++ src/main/java/com/example/creditservice/CreditResponse.java
@@ -0,0 +1,14 @@
+package com.example.creditservice;
+
+public class CreditResponse {
+
+    private Integer creditScore;
+
+    public Integer getCreditScore() {
+        return creditScore;
+    }
+
+    public void setCreditScore(Integer creditScore) {
+        this.creditScore = creditScore;
+    }
+}
